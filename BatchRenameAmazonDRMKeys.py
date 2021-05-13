556760bc2ef26c24a746d425efd97c8397905a71
heray1990 <heray1990@hotmail.com>
2021-05-13 15:57:48 +0800

    1. 增加 Output 目录, 先把原始文件依次复制到 Output 目录, 然后在 Output 下重命名文件

--- BatchRenameAmazonDRMKeys.py
+++ BatchRenameAmazonDRMKeys.py
@@ -9,27 +9,34 @@
 #  This example rename files, which are in 'HDCP' directory, from 'CtvHDCPKey_*.bin' to 'HDCP2_*.bin'.
 #  And save new renamed files into 'Output' directory.
 
-import glob, os, sys
-#import shutil
+import re, os, sys
+import shutil
 
 HECTOR_FAIRPLAY_KEY_PREFIX = "FAIRPLAYMT9021000000HS"
 HECTOR_WIDEVINE_KEY_PREFIX = "WIDEVNMTK90210000000HS"
+OUTPUT_DIR_NAME = "Output"
 
-def rename(outDir, keyType, strStartNum):
-    if keyType == 'fairplay':
-        strRe = r'*fairplay*'
-    elif keyType == 'widevine':
-        strRe = r'*widevine*'
-    else:
-        print(r'Please confirm which kind of key you want to rename. Only support fairplay or widevine of Hector right.')
-        sys.exit(1)
+def rename(inDir, outDir, keyType, strStartNum):
+    #if keyType == 'fairplay':
+    #    strRe = r'*fairplay*'
+    #elif keyType == 'widevine':
+    #    strRe = r'*widevine*'
+    #else:
+    #    print(r'Please confirm which kind of key you want to rename. Only support fairplay or widevine of Hector right.')
+    #    sys.exit(1)
 
     intStartNum = int(strStartNum)
-    for pathAndFilename in glob.iglob(os.path.join(outDir, strRe)):
-        #print('%d' % len(str(intStartNum)))
+    for pathAndFilename in os.listdir(inDir):
+        match = re.search(keyType, str(pathAndFilename))
+        if not match:
+            print("%s not match" % strRe)
+            continue
+
         if intStartNum > 99999999:
             print(r'The number is out of range. It should be from 0 to 99999999.')
             sys.exit(1)
+
+        shutil.copy(os.path.join(inDir, pathAndFilename), outDir)
 
         if len(str(intStartNum)) == 1:
             strStartNumTmp = '0000000' + str(intStartNum)
@@ -53,7 +60,7 @@ def rename(outDir, keyType, strStartNum):
         elif keyType == 'widevine':
             strOutputFileName = HECTOR_WIDEVINE_KEY_PREFIX + strStartNumTmp + r'.bin'
 
-        os.rename(pathAndFilename, os.path.join(outDir, strOutputFileName))
+        os.rename(os.path.join(outDir, pathAndFilename), os.path.join(outDir, strOutputFileName))
         intStartNum = intStartNum + 1
         print('%s -> %s' % (os.path.basename(pathAndFilename), strOutputFileName))
 
@@ -66,16 +73,16 @@ def main():
         print(r'The starting number should be from 0 to 99999999')
         sys.exit(1)
 
-    #inDir = sys.argv[1]
-    #outDir = os.path.join(os.getcwd(), 'Output')
-    #if os.path.exists(outDir):
-    #    print("Output folder already exist. Delete it.")
-    #    shutil.rmtree(outDir)
+    inDir = sys.argv[1]
+    outDir = os.path.join(os.getcwd(), OUTPUT_DIR_NAME)
+    if os.path.exists(outDir):
+        print("Output folder already exist. Delete it.")
+        shutil.rmtree(outDir)
 
-    #print("Greate Output folder and copy files from Input to Output folder.")
-    #shutil.copytree(inDir, outDir)
+    print("Greate Output folder.")
+    os.mkdir(outDir)
 
-    rename(sys.argv[1], sys.argv[2], sys.argv[3])
+    rename(inDir, outDir, sys.argv[2], sys.argv[3])
 
 if __name__=='__main__':
     main()
